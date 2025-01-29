17fc296bd1040004a08decb45b1f6ae58eddb70c
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2025-01-29 11:50:45 +0600

    add middleware

---
+++ src/redux/middlewares/logger.ts
@@ -0,0 +1,12 @@
+import { act } from "react";
+
+const logger = (state) => (next) => (action) =>{
+    console.group(action.type);
+    console.info("prev state ",state.getState())
+    const result  = next(action)
+    console.info("Next state=> ",state.getState())
+    console.groupEnd()
+    return result
+};
+
+export default logger;
